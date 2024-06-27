f9578ebaf24c794c4b07aa654b6addf81a092590
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-06-27 20:45:21 -0300

    Criando Exercicios

--- POO-Bootcamp-Giulia/POO-Bootcamp-Giulia/main.swift
+++ POO-Bootcamp-Giulia/POO-Bootcamp-Giulia/main.swift
@@ -106,3 +106,83 @@ class Celular {
 var iphone: Celular = Celular(modelo: "Iphone 15", preco: 8000, cor: "Preto")
 
 
+\//MARK: Se caso NÃO TEM VALOR INICIAL, VOCÊ PRECISA PASSAR ELE NO INIT!!!!!!
+//MARK: O mesmo se encaixa para tipagens opcionais, POIS o opcional EXISTE ALGO DE INICIO -> QUE É O NIL! Com base nisso
+//MARK: não é obrigado a passar ele no construtor. Mas caso queira, sem problemas!
+class Bolo {
+
+  var peso: Double
+  var preco: Double
+  var sabor: [String]
+  var topoDeBolo: Bool? = nil
+
+  init(peso: Double, preco: Double, sabor: [String]) {
+    self.peso = peso
+    self.preco = preco
+    self.sabor = sabor
+  }
+
+  func adicionarSabor(sabor: String) {
+    self.sabor.append(sabor)
+  }
+}
+
+var boloAniversario: Bolo = Bolo(peso: 10, preco: 200, sabor: ["Chocolate", "Baunilha"])
+var boloCasamento: Bolo = Bolo(peso: 50, preco: 2000, sabor: ["Chocolate", "Baunilha", "Maça"])
+
+// MARK: - Exercicios Classe
+
+// Exercício 1: Conta Poupança
+// Objetivo: Desenvolver uma classe chamada 'ContaPoupanca' para gerenciar os depósitos em uma conta poupança de banco.
+//
+// Descrição:
+// Propriedades:
+// - saldo (Double): O saldo atual na conta.
+// Construtor:
+// - Inicialize o saldo.
+// Métodos:
+// - depositar(valor: Double): Adiciona(+) o valor especificado ao saldo atual. (saldo = saldo + valor especificado)
+// - consultarSaldo() -> Double: Retorna o saldo atual da conta.
+
+//   Para usar este método, você deve criar uma instância da classe, chamar este método e armazenar o valor retornado em uma variável. Em seguida, você pode imprimir esse valor.
+// print("Saldo atual: R$\(saldoAtual)")
+
+
+
+
+
+// Exercício 2: Gerenciador de Filmes
+// Objetivo: Desenvolver uma classe chamada 'Filme' para gerenciar informações sobre filmes em um acervo pessoal.
+//
+// Descrição:
+// Propriedades:
+// - titulo (String): O título do filme.
+// - diretor (String): O diretor do filme.
+// - ano (Int): O ano de lançamento do filme.
+// Construtor:
+// - Inicialize titulo, diretor e ano.
+// Métodos:
+// - exibirInfo() -> String: Retorna uma string com todas as informações do filme formatadas.
+
+//   Para usar este método, crie uma instância da classe, chame o método e armazene a informação retornada em uma variável. Em seguida, imprima essa variável para visualizar os detalhes do filme.
+
+
+
+
+
+// Exercício 3: Sistema de Registro de Alunos
+// Objetivo: Criar uma classe chamada 'Aluno' para armazenar dados e calcular a média de notas de alunos em uma escola.
+//
+// Descrição:
+// Propriedades:
+// - nome (String): O nome do aluno.
+// - notas (Array<Double>): Uma lista de notas do aluno.
+// Construtor:
+// - Inicialize nome e notas.
+// Métodos:
+// - calcularMedia() -> Double: Calcula e retorna a média das notas do aluno utilizando um laço de repetição para somar as notas.
+
+//   Para utilizar este método, crie uma instância da classe, invoque o método para calcular a média, armazene esse valor em uma variável e imprima a média para visualizar.
+// print("Média das notas de \(aluno.nome): \(media)")
+
+
