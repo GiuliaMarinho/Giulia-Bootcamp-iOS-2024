6f32d95a48da96217cd0000732b91d7857b5994a
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-06-27 22:21:39 -0300

    Correcao exercicios

--- POO-Bootcamp-Giulia/POO-Bootcamp-Giulia/main.swift
+++ POO-Bootcamp-Giulia/POO-Bootcamp-Giulia/main.swift
@@ -16,29 +16,29 @@ import Foundation
 // Utilizamos a palavra reservada 'class' e na sequencia seu nome sendo, SEMPRE COM A PRIMEIRA LETRA MAIUSCULA.
 
 class Automovel {
-
-  let cor: String = "Vermelho"
-  let quantidadeDeEspelho: Int = 5
-  let marca: String = "Ferrari"
-  let tipoCombustivel: String = "Alcool"
-  let tipoMotor: String = "AP"
-  let potencia: Double = 2.0
-  let conversivel: Bool = true
-  var cavaloDePotencia: Int = 900
-
-  func ligarCarro() {
-    print("O carro da marca \(marca) está ligado")
-  }
-
-  func acenderFarois() {
-    print("As luzes estão ligadas")
-  }
-
-  func detalheCompletoAutomovel() -> String {
-    let texto: String = "O veiculo da marca: \(marca) tem o motor: \(tipoMotor) que utiliza o combustivel \(tipoCombustivel) e tem um total de \(cavaloDePotencia) cavalos de potencia"
-    return texto
-  }
-
+    
+    let cor: String = "Vermelho"
+    let quantidadeDeEspelho: Int = 5
+    let marca: String = "Ferrari"
+    let tipoCombustivel: String = "Alcool"
+    let tipoMotor: String = "AP"
+    let potencia: Double = 2.0
+    let conversivel: Bool = true
+    var cavaloDePotencia: Int = 900
+    
+    func ligarCarro() {
+        print("O carro da marca \(marca) está ligado")
+    }
+    
+    func acenderFarois() {
+        print("As luzes estão ligadas")
+    }
+    
+    func detalheCompletoAutomovel() -> String {
+        let texto: String = "O veiculo da marca: \(marca) tem o motor: \(tipoMotor) que utiliza o combustivel \(tipoCombustivel) e tem um total de \(cavaloDePotencia) cavalos de potencia"
+        return texto
+    }
+    
 }
 
 
@@ -63,19 +63,19 @@ print(uno.cavaloDePotencia)
 //MARK: - Construtores
 
 class Pessoa {
-  var nome: String
-  var idade: Int
-  var peso: Double
-  var altura: Double
-
-  // Método Construtor -> Ele é quem seta/informa os valores iniciais!!!
-  init(nome: String, idade: Int, peso: Double, altura: Double) {
-    self.nome = nome
-    self.idade = idade
-    self.peso = peso
-    self.altura = altura
-  }
-
+    var nome: String
+    var idade: Int
+    var peso: Double
+    var altura: Double
+    
+    // Método Construtor -> Ele é quem seta/informa os valores iniciais!!!
+    init(nome: String, idade: Int, peso: Double, altura: Double) {
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+    }
+    
 }
 
 // Neste exemplo, estamos gerando a instancia de uma forma diferente -> a instancia é gerada QUANDO preenchido todas as propriedades
@@ -90,41 +90,42 @@ print(pedro.peso)
 
 // Se caso NÃO TEM VALOR INICIAL, VOCÊ PRECISA PASSAR ELE NO INIT!!!!!!
 class Celular {
-  var valorPiCelular: Double = 3.14
-  var tamanho: Double = 1.432432
-  var cor: String
-  var preco: Double
-  var modelo: String
-
-  init(modelo: String, preco: Double, cor: String) {
-    self.modelo = modelo
-    self.preco = preco
-    self.cor = cor
-  }
+    var valorPiCelular: Double = 3.14
+    var tamanho: Double = 1.432432
+    var cor: String
+    var preco: Double
+    var modelo: String
+    
+    init(modelo: String, preco: Double, cor: String) {
+        self.modelo = modelo
+        self.preco = preco
+        self.cor = cor
+    }
 }
 
 var iphone: Celular = Celular(modelo: "Iphone 15", preco: 8000, cor: "Preto")
 
 
-\//MARK: Se caso NÃO TEM VALOR INICIAL, VOCÊ PRECISA PASSAR ELE NO INIT!!!!!!
+//MARK: Se caso NÃO TEM VALOR INICIAL, VOCÊ PRECISA PASSAR ELE NO INIT!!!!!!
 //MARK: O mesmo se encaixa para tipagens opcionais, POIS o opcional EXISTE ALGO DE INICIO -> QUE É O NIL! Com base nisso
 //MARK: não é obrigado a passar ele no construtor. Mas caso queira, sem problemas!
+
 class Bolo {
-
-  var peso: Double
-  var preco: Double
-  var sabor: [String]
-  var topoDeBolo: Bool? = nil
-
-  init(peso: Double, preco: Double, sabor: [String]) {
-    self.peso = peso
-    self.preco = preco
-    self.sabor = sabor
-  }
-
-  func adicionarSabor(sabor: String) {
-    self.sabor.append(sabor)
-  }
+    
+    var peso: Double
+    var preco: Double
+    var sabor: [String]
+    var topoDeBolo: Bool? = nil
+    
+    init(peso: Double, preco: Double, sabor: [String]) {
+        self.peso = peso
+        self.preco = preco
+        self.sabor = sabor
+    }
+    
+    func adicionarSabor(sabor: String) {
+        self.sabor.append(sabor)
+    }
 }
 
 var boloAniversario: Bolo = Bolo(peso: 10, preco: 200, sabor: ["Chocolate", "Baunilha"])
@@ -147,9 +148,26 @@ var boloCasamento: Bolo = Bolo(peso: 50, preco: 2000, sabor: ["Chocolate", "Baun
 //   Para usar este método, você deve criar uma instância da classe, chamar este método e armazenar o valor retornado em uma variável. Em seguida, você pode imprimir esse valor.
 // print("Saldo atual: R$\(saldoAtual)")
 
+class ContaPoupanca {
+    var saldo: Double
+    
+    init(saldo: Double) {
+        self.saldo = saldo
+    }
 
+    func depositar(valor: Double) {
+        saldo = saldo + valor
+    }
+    
+    func consultarSaldo()-> Double {
+        return saldo
+    }
+}
 
-
+var conta: ContaPoupanca = ContaPoupanca(saldo: 550.00 )
+print(conta.consultarSaldo())
+conta.depositar(valor: 550.00)
+print(conta.consultarSaldo())
 
 // Exercício 2: Gerenciador de Filmes
 // Objetivo: Desenvolver uma classe chamada 'Filme' para gerenciar informações sobre filmes em um acervo pessoal.
@@ -166,8 +184,26 @@ var boloCasamento: Bolo = Bolo(peso: 50, preco: 2000, sabor: ["Chocolate", "Baun
 
 //   Para usar este método, crie uma instância da classe, chame o método e armazene a informação retornada em uma variável. Em seguida, imprima essa variável para visualizar os detalhes do filme.
 
+class Filme {
+    var titulo: String
+    var diretor: String
+    var ano: Int
+    
+    init(titulo: String, diretor: String, ano: Int) {
+        
+        self.titulo = titulo
+        self.diretor = diretor
+        self.ano = ano
+    }
+    
+    func exibirInfo() -> String {
+        var informacoes: String = " O filme \(titulo) foi ao ar pelo diretor \(diretor), no ano de \(ano)"
+        return informacoes
+    }
+}
 
-
+var filme2: Filme = Filme(titulo: "Diarios de um vampiro", diretor: "Julie Plec", ano: 2009)
+print(filme2.exibirInfo())
 
 
 // Exercício 3: Sistema de Registro de Alunos
@@ -185,4 +221,26 @@ var boloCasamento: Bolo = Bolo(peso: 50, preco: 2000, sabor: ["Chocolate", "Baun
 //   Para utilizar este método, crie uma instância da classe, invoque o método para calcular a média, armazene esse valor em uma variável e imprima a média para visualizar.
 // print("Média das notas de \(aluno.nome): \(media)")
 
+
+class Aluno {
+    
+    var nome: String
+    var notas: [Double]
+    
+    init(nome: String, notas: [Double]) {
+        self.nome = nome
+        self.notas = notas
+    }
+    
+    func calcularMedia() -> Double {
+        var soma: Double = 0
+        for nota in notas {
+            soma = soma + nota
+        }
+        return soma / Double(notas.count)
+    }
+}
+
+var alunaGiulia: Aluno = Aluno(nome: "Giulia", notas: [7,6,5,4])
+print("Media das notas de \(alunaGiulia.nome): \(alunaGiulia.calcularMedia())")
 
