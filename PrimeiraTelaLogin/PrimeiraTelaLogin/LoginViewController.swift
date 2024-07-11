5f7c4096f0212ee433d7a922a7d43726691b6858
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-07-10 23:00:19 -0300

    ok

---
+++ PrimeiraTelaLogin/PrimeiraTelaLogin/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeiraTelaLogin
+//
+//  Created by Giulia Marinho on 10/07/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func configElements() {
+        
+    }
+
+}
+
