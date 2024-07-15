06ba50bab1906f84615d6796c03b1a7043a6b7e7
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-07-14 22:35:23 -0300

    Atualizando

--- Desafio-Tela-de-Login/Desafio-Tela-de-Login/ViewController.swift
+++ Desafio-Tela-de-Login/Desafio-Tela-de-Login/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       configElements()
+        configElements()
     }
     
     func configElements() {
@@ -24,17 +24,50 @@ class ViewController: UIViewController {
         emailTextField.placeholder = "Digite seu email:"
         emailTextField.keyboardType = .emailAddress
         emailTextField.autocorrectionType = .no
+        emailTextField.delegate = self
         emailTextField.layer.borderColor = UIColor.lightGray.cgColor
         
         passwordTextField.placeholder = "Digite sua senha:"
         passwordTextField.autocorrectionType = .no
-        passwordTextField.isSecureTextEntry = true
+        passwordTextField.isSecureTextEntry = false
+        passwordTextField.delegate = self
         passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
-
+        
         confirmthePassword.placeholder = "Confirme sua senha:"
         confirmthePassword.autocorrectionType = .no
-        confirmthePassword.isSecureTextEntry = true
+        confirmthePassword.isSecureTextEntry = false
+        confirmthePassword.delegate = self
         confirmthePassword.layer.borderColor = UIColor.lightGray.cgColor
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+        textField.layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+      func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        textField.resignFirstResponder()
+        return true
+      }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailPattern)
+        return emailPred.evaluate(with: email)
+    }
+    
+    func isValidPassword(_ password: String) -> Bool {
+        let passwordPattern = ".{6,}"
+        let passwordPred = NSPredicate(format: "SELF MATCHES %@", passwordPattern)
+        return passwordPred.evaluate(with: password)
+    }
+}
