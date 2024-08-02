27bd058a5d72313037737378b5dc1f267f9f6e76
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-08-01 22:56:04 -0300

    Collection

---
+++ CollectionView/CollectionView/List/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Giulia Marinho on 01/08/24.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+      
+    }
+
+}
+
---
+++ CollectionView/CollectionView/List/Screen/ListScreen.swift
@@ -0,0 +1,25 @@
+//
+//  ListScreen.swift
+//  CollectionView
+//
+//  Created by Giulia Marinho on 01/08/24.
+//
+
+import UIKit
+
+class ListScreen: UIView {
+
+    
+    
+    init() {
+      super.init(frame: .zero)
+      backgroundColor = .red
+      
+    }
+
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
+
+}
