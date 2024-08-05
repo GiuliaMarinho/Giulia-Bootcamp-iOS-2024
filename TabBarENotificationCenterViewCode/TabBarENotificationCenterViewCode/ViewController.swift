f8b95f2836ac3cf242fdd7809c95ca4e7359f96a
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-08-05 19:34:48 -0300

    Criando tab bar

---
+++ TabBarENotificationCenterViewCode/TabBarENotificationCenterViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarENotificationCenterViewCode
+//
+//  Created by Giulia Marinho on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
