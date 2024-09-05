7d446515e62370a8ad7e8fcccc234437d250a9b4
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-09-04 22:46:48 -0300

    Solucionando Exercicios decodable e encondable

--- Json-Codable-Decodable-Encodable.playground/Contents.swift
+++ Json-Codable-Decodable-Encodable.playground/Contents.swift
@@ -166,3 +166,234 @@ let veiculo = Veiculo(
     ]
 )
 
+// 3 -
+
+//{
+//    "id": 101,
+//    "marca": "Toyota",
+//    "modelo": "Corolla",
+//    "ano": 2022,
+//    "acessorios": [
+//        {
+//            "nome": "Teto solar",
+//            "preco": 2500.00,
+//            "configuracaoAcessorio": {
+//                "tipo": "Luxo",
+//                "dataCriacao": "2021-06-15"
+//            }
+//        },
+//        {
+//            "nome": "Multimídia",
+//            "preco": 1200.00,
+//            "configuracaoAcessorio": {
+//                "tipo": "Tecnologia",
+//                "dataCriacao": "2021-08-20"
+//            }
+//        }
+//    ]
+//}
+
+
+
+// MARK: - Decodable
+
+// Decodable é um protocolo em Swift que permite que você converta dados no formato JSON em um objeto Swift. Este processo é conhecido como decodificação.
+
+// OBS: Quando tentar realizar o decode(parse) ele vai olhar sempre buscando a chave e a tipagem se ambos são IGUAIS! Caso não sejam iguais, ele cai no catch.
+// Tudo oque tem no MODELO SWIFT que não seja opcional, ele se torna algo OBRIGATORIO!! Caso o back não retorne aquele campo, ele vai cair no catch. Caso a variavel seja opcional, ai não tem problema, pois seu valor será nil
+
+
+struct House: Decodable {
+  var address: String
+  var numberOfRooms: Int
+  var hasGarden: Bool
+}
+
+let jsonString = """
+{
+  "address": "Avenida 4562 bairro jardim europa",
+  "numberOfRooms": 23435,
+  "hasGarden": false
+}
+"""
+
+if let jsonData = jsonString.data(using: .utf8) {
+  do {
+    let house = try JSONDecoder().decode(House.self, from: jsonData)
+    print("Decodificado com sucesso!!")
+    print(house.address)
+    print(house.numberOfRooms)
+    print(house.hasGarden)
+  } catch  {
+    // Ele tenta (try) decodificar, se caso não conseguir, ele cai no caso do catch
+    print("Error ao decodificar o JSON: \(error.localizedDescription)")
+  }
+}
+
+
+// MARK: - CodingKey
+
+struct Animal: Decodable {
+  var species: String
+  var age: Int
+  var habitatType: String
+  var isDangerous: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case species
+    case age
+    case habitatType = "habitat_type"
+    case isDangerous = "is_dangerous"
+  }
+}
+
+let jsonStringAnimal = """
+{
+ "species" : "Tigre",
+ "age": 1,
+ "habitat_type": "Floresta",
+ "is_dangerous": true
+}
+"""
+
+if let jsonData = jsonStringAnimal.data(using: .utf8) {
+  do {
+    let animal = try JSONDecoder().decode(Animal.self, from: jsonData)
+    print("Animal Decodificado com sucesso!!")
+    print(animal.species)
+    print(animal.age)
+    print(animal.habitatType)
+    print(animal.isDangerous)
+  } catch  {
+    // Ele tenta (try) decodificar, se caso não conseguir, ele cai no caso do catch
+    print("Error ao decodificar o JSON: \(error.localizedDescription)")
+  }
+}
+
+// MARK: Encodable
+
+// Encodable é outro protocolo em Swift. Ele permite que você faça o oposto do que o Decodable faz: você pode converter um objeto Swift em dados no formato JSON. Isso é chamado de codificação.
+
+struct Dog: Encodable {
+  var name: String
+  var age: Int
+  var breed: String
+  var isVaccinated: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case name
+    case age
+    case breed
+    case isVaccinated = "is_vaccinated"
+  }
+}
+
+let myDog = Dog(name: "Ayron", age: 1, breed: "Samoieda", isVaccinated: true)
+
+do {
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted // facilita a leitura
+  let jsonData = try encoder.encode(myDog)
+  if let jsonString = String(data: jsonData, encoding: .utf8) {
+    print(jsonString)
+  }
+} catch {
+  print("Error ao codificar o JSON: \(error.localizedDescription)")
+}
+
+
+// MARK: - Desafios
+
+// 1- Faça o Decodable desse json
+
+struct Film: Decodable {
+  var title: String
+  var director: String
+  var releaseYear: Int
+}
+
+let filmJson = """
+{
+   "title": "Inception",
+   "director": "Christopher Nolan",
+   "releaseYear": 2010
+}
+
+"""
+
+
+if let jsonData = filmJson.data(using: .utf8) {
+  do {
+    let Film = try JSONDecoder().decode(Film.self, from: jsonData)
+    print("Decodificado com sucesso!!")
+    print(Film.title)
+    print(Film.director)
+    print(Film.releaseYear)
+  } catch  {
+    print("Error ao decodificar o JSON: \(error.localizedDescription)")
+  }
+}
+
+
+
+// 2 - Faça o Encodable desse modelo
+
+struct Student: Encodable {
+    var name: String
+    var age: Int
+    var grades: [Int]
+    
+}
+
+let myPerson = Student(name: "Giulia", age: 22, grades: [10] )
+
+do {
+  let encoder = JSONEncoder()
+  encoder.outputFormatting = .prettyPrinted
+  let jsonData = try encoder.encode(myPerson)
+  if let jsonString = String(data: jsonData, encoding: .utf8) {
+    print(jsonString)
+  }
+} catch {
+  print("Error ao codificar o JSON: \(error.localizedDescription)")
+}
+
+// 3 - Faça o Decode funcionar
+
+//OBS: Você recebeu um JSON de uma API que contém detalhes sobre um veículo. O JSON tem 10 campos, mas você precisa extrair e usar apenas 4 deles: model, make, year, e color. Durante a decodificação, você encontrará alguns erros intencionais que precisam ser corrigidos.
+
+struct Vehicle: Decodable {
+    var model: String
+    var make: String
+    var year: String
+    var color: String
+
+}
+
+let jsonStringVehicle = """
+{
+    "model": "Explorer",
+    "make": "Ford",
+    "year": "2020",
+    "color": "Blue",
+    "engine": "3.5L V6",
+    "seats": 7,
+   "type": "SUV",
+   "milage": "12000",
+   "price": "35000",
+    "airConditioned": true
+}
+"""
+
+if let jsonData = jsonStringVehicle.data(using: .utf8) {
+  do {
+    let decoder = JSONDecoder()
+    let vehicle = try decoder.decode(Vehicle.self, from: jsonData)
+      print("Decodificado com sucesso!!")
+      print(vehicle)
+  } catch {
+    print("Erro ao decodificar o Vehicle: \(error)")
+  }
+}
+
+
