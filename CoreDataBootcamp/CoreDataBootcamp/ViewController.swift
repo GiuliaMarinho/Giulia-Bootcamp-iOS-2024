6fa791cb168375652fb20ba572b18c7f4397c655
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-10-07 22:33:14 -0300

    Correcao

---
+++ CoreDataBootcamp/CoreDataBootcamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataBootcamp
+//
+//  Created by Giulia Marinho on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+
+
+}
+
